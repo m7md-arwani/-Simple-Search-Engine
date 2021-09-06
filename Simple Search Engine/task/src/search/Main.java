dc71a9eae35da7b611f06fe864889d8794888630
m7md-arwani <moh.3ara@hotmail.com>
2021-09-06 15:03:32 +0300

    The project is functioning, but it's far from ok in terms of OOP. This to be fixed in the next stage. Stage(6)

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -57,7 +57,7 @@ public class Main {
             }
         }
     }
-
+    // Any method
     public static void search() {
 
 
@@ -72,6 +72,76 @@ public class Main {
                 System.out.println(data[LineNumbers]);
             }
         }
+
+
+    }
+
+    public static void ANY_search() {
+        HashSet<Integer> num = new HashSet<>();
+        System.out.println("Enter a name or email to search all suitable");
+        String [] str = g.nextLine().trim().toLowerCase().split(" ");
+        for (String st: str) {
+            num.addAll(map.get(st));
+        }
+        Object[] linesNum = num.toArray();
+        System.out.println(num.size() + " persons found:");
+        for(int i = 0 ; i < linesNum.length; i++) {
+            System.out.println(data[(int) linesNum[i]]);
+        }
+
+    }
+
+    public static void ALL_search() {
+        System.out.println("Enter a name or email to search all suitable");
+        String [] str = g.nextLine().trim().toLowerCase().split(" ");
+        List<String> database = new ArrayList<>(List.of(data));
+        List<String> results = new ArrayList<>();
+
+      for (String line : database) {
+          if(line.toLowerCase().contains(str[0]) && line.toLowerCase().contains(str[1]) && line.toLowerCase().contains(str[2])) {
+            results.add(line);
+          }
+      }
+      if (results.size() == 0) {
+          System.out.println("No matching people found.");
+
+      } else {
+          System.out.println(results.size() + " persons found:");
+          for (String line: results) {
+              System.out.println(line);
+          }
+
+      }
+
+
+
+
+
+
+
+
+    }
+
+    public static void NONE_search() {
+        System.out.println("Enter a name or email to search all suitable");
+        String [] str = g.nextLine().trim().toLowerCase().split(" ");
+        HashSet<Integer> linesToDelete = new HashSet<>();
+        List<String> results = new ArrayList<>();
+        for(String word: str) {
+            linesToDelete.addAll(map.get(word));
+        }
+        for (int i = 0 ; i < data.length; i++) {
+            if (!linesToDelete.contains(i)) {
+             results.add(data[i]);
+            }
+        }
+        System.out.println(results.size() + " persons found:");
+        for (String line: results) {
+            System.out.println(line);
+        }
+
+
+
 
 
     }
@@ -124,7 +194,22 @@ class state extends Main {
                 break;
             // From here, the cases will act according to the state that have been changed in the previous switch.
             case FIND_A_PERSON:
-                Main.search();
+                System.out.println("Select a matching strategy: ALL, ANY, NONE");
+                String str = g.nextLine();
+                switch (str) {
+                    case "ANY":
+                        Main.ANY_search();
+                        break;
+                    case "ALL":
+                        Main.ALL_search();
+                        break;
+
+                    case "NONE" :
+                         Main.NONE_search();
+                        break;
+                    default:
+                        System.out.println("Wrong");
+                }
                 currentState = State.MENU;
                 break;
             case PRINT_ALL_PEOPLE:
